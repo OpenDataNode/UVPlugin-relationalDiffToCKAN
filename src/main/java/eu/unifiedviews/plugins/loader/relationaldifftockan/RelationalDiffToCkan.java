072ebe8a848ba2ae99e34574ccac9761d3fe434f
SvetoKrchnavy <krchnavy@eea.sk>
2015-05-22 14:46:44 +0200

    tuning of fix #2 - there was no release with dpu.uv-l-rdfToCkan.http.header.[key] so no backward compatibility necessary.

--- src/main/java/eu/unifiedviews/plugins/loader/relationaldifftockan/RelationalDiffToCkan.java
+++ src/main/java/eu/unifiedviews/plugins/loader/relationaldifftockan/RelationalDiffToCkan.java
@@ -115,12 +115,6 @@ public class RelationalDiffToCkan extends AbstractDpu<RelationalDiffToCkanConfig
     @Deprecated
     public static final String CONFIGURATION_DPU_CATALOG_API_URL = "dpu.uv-l-relationalDiffToCkan.catalog.api.url";
 
-    /**
-     * @deprecated Global configuration should be used {@link CONFIGURATION_HTTP_HEADER}
-     */
-    @Deprecated
-    public static final String CONFIGURATION_DPU_HTTP_HEADER = "dpu.uv-l-relationalDiffToCkan.http.header.";
-
     public static final String CONFIGURATION_SECRET_TOKEN = "org.opendatanode.CKAN.secret.token";
 
     public static final String CONFIGURATION_CATALOG_API_URL = "org.opendatanode.CKAN.api.url";
@@ -169,16 +163,6 @@ public class RelationalDiffToCkan extends AbstractDpu<RelationalDiffToCkanConfig
                 String headerName = configEntry.getKey().replace(CONFIGURATION_HTTP_HEADER, "");
                 String headerValue = configEntry.getValue();
                 additionalHttpHeaders.put(headerName, headerValue);
-            }
-        }
-        if (additionalHttpHeaders.isEmpty()) {
-            LOG.debug("Missing global configuration for additional HTTP headers, trying to use DPU specific configuration");
-            for (Map.Entry<String, String> configEntry : environment.entrySet()) {
-                if (configEntry.getKey().startsWith(CONFIGURATION_DPU_HTTP_HEADER)) {
-                    String headerName = configEntry.getKey().replace(CONFIGURATION_DPU_HTTP_HEADER, "");
-                    String headerValue = configEntry.getValue();
-                    additionalHttpHeaders.put(headerName, headerValue);
-                }
             }
         }
 
